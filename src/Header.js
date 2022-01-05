6d4c07461d77755f78c01d614b3c13fe3e022fb2
saumyagupta07 <saumya.gupta@medstokk.com>
2022-01-05 18:57:40 +0530

    Create a React app and publish it to GitHub Pages

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function Header() {
+    return (
+        <div>
+           <img className='logo' src="https://uploads-ssl.webflow.com/6036972e08af561b470b4831/6135c64b57cf212f2aa106f1_Peppertype-logo-dark-horizontal.svg" alt="logo" />
+        </div>
+    )
+}
+
+export default Header
---
+++ src/Main.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Main() {
+    return (
+        <div className='main'>
+            <div className="hi">Hi Siva 👋</div>
+            <div className="welcome">Welcome to Peppertype.ai</div>
+            <p className="para1">Congratulations on taking your first step towards creating content, the modern way!</p>
+            <p className="para2">Help us tailor an amazing experience for you by answering these three questions</p>
+
+            <div className="btn">
+                <button className="skip">Skip for now</button>
+                <button className="continue">Continue</button>
+            </div>
+            <div className="extra">Get the most out of it - <u>Book a live demo</u></div>
+        </div>
+    )
+}
+
+export default Main
